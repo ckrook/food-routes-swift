6321d966ce90e8550175384fc50337a390124cc2
Chad Timmerman <chad.timmerman@punchkick.com>
2025-02-07 14:12:48 +0100

    Xcode Project Creation

---
+++ Routes/Routes/RoutesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoutesApp.swift
+//  Routes
+//
+//  Created by Chad Timmerman on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct RoutesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
